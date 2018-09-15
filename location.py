11362922c5fd7fa47eaa10e57b9ac5826c9290cf
Alex Zhao <alexzhao21@gmail.com>
2018-09-15 19:57:42 -0400

    add location endpt

--- location.py
+++ location.py
@@ -1,9 +1,12 @@
 from flask import *
 from api import db
 
-def add_new_location(username, coordinates):
-    user = db.find_one({"username": username})
-    if user is not None:
+def add_new_location(username, lat, lng):
+    user = db.users.find_one({"username": username})
+    if user is None:
         return jsonify(success=False, reason="User does not exist")
     
-    
+    db.users.update_one({"username": username}, {"$push": {"pathHistory": [lat, lng]}})
+
+    return jsonify(success=True)
+
