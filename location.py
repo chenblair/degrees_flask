5bab8a81a22000736eb4bd6627cf811c1c6791fa
Alex Zhao <alexzhao21@gmail.com>
2018-09-15 19:42:31 -0400

    debug

--- location.py
+++ location.py
@@ -2,4 +2,8 @@ from flask import *
 from api import db
 
 def add_new_location(username, coordinates):
-    return None
+    user = db.find_one({"username": username})
+    if user is not None:
+        return jsonify(success=False, reason="User does not exist")
+    
+    
